35c56959727abc50a395305d4613c4e01bee0ce1
Yujie Liang <113095482+vg219@users.noreply.github.com>
2025-08-24 17:25:52 +0800

    Refactor model registry to include EDACANet

--- model/__init__.py
+++ model/__init__.py
@@ -22,43 +22,11 @@ __all__ = [
 ]
 
 _all_modules = [
-    'panrwkv_v8_cond_norm',
-    'panRWKV_v9_local',
-    'RWKVFusion_v10_multi_modal',
-    'RWKVFusion_v12',
-    'MHIIF',
-    'MHIIF_g',
-    'FeINFN',
-    'hermite',
-    'MHIIF_J',
-    'MHIIF_J2',
-    'MHIIF_rbf',
-    'ENACIR',
-    'ENACIR_V2',
-    'MHIIF_J2_Hermite',
-    'MIMO_SST',
-    'DHIF',
-    'DCT',
+    'EDACANet',
 ]
 
 _all_model_class_name = [
-    'RWKVFusion',
-    'RWKVFusion',
-    'RWKVFusion',
-    'RWKVFusion',
-    'MHIIF_',
-    'MHIIF_g',
-    'FeINFNet',
-    'hermite',
-    'MHIIF_J',
-    'MHIIF_J2',
-    'MHIIF_rbf',
-    'ENACIR',
-    'ENACIR_V2',
-    'MHIIF_J2_Hermite',
-    'Net',
-    'HSI_Fusion',
-    'DCT',
+    'EDACANet',
 ]
 
 assert len(_all_modules) == len(_all_model_class_name), 'length of modules and registry names should be the same'
@@ -90,83 +58,6 @@ def _active_load_module(module_name, model_class_name):
     else:
         logger.critical(f'{dict_name} not found in LAZY_LOADER_DICT')
         raise ValueError(f'{dict_name} not found in LAZY_LOADER_DICT')
-
-
-# TODO: hydra import
-# ==============================================
-# register all models
-# from model.DCFNet import DCFNet
-# from model.FusionNet import FusionNet
-# from model.PANNet import VanillaPANNet
-
-# from model.M3DNet import M3DNet
-# from model.panformer import PanFormerGAU, PanFormerUNet2, PanFormerSwitch, PanFormerUNet, PanFormer
-# from model.dcformer import DCFormer
-# from model.dcformer_dpw import DCFormer_DPW
-# from model.dcformer_dpw_woo import DCFormer_DPW_WOO
-# from model.dcformer_dynamic import DCFormerDynamicConv
-# from model.dcformer_reduce import DCFormer_Reduce
-# from model.dcformer_mwsa import DCFormerMWSA
-# from model.dcformer_mwsa_wx import DCFormerMWSA
-
-# from model.fuseformer import MainNet
-
-# from model.dcformer_reduce_c_64 import DCFormer_Reduce_C64
-# from model.dcformer_reduce_c_32_tmp import DCFormer_Reduce_C32
-# from model.dcformer_sg_c32 import DCFormer_SG_C32
-# from model.dcformer_mobile_x8 import DCFormerMobile
-# from model.ydtr import MODEL as YDTR
-# from model.CSSNet import Our_netf
-# from model.mmnet import MMNet
-# from model.pmacnet import PMACNet
-# from model.SSRNet import SSRNET
-# from model.hsrnet import HSRNet
-# from model.restfnet import ResTFNet
-# from model.HPMNet import fusionnet
-
-# ablation
-# from model.dcformer_abla_only_channel_attn import DCFormer_XCA
-# from model.dcformer_abla_only_mwa import DCFormerOnlyMWA
-# from model.dcformer_abla_only_cross_branch_mwsa import DCFormerOnlyCrossBranchMWSA
-# from model.ablation_exps.dcformer_abla_wo_ghost_module import DCFormerMWSA
-# from model.ablation_exps.dcformer_abla_only_XCA import DCFormerMWSA
-# from model.ablation_exps.dcformer_abla_only_MWSA import DCFormerMWSA
-# from model.ablation_exps.dcformer_abla_in_scale_MWSA import DCFormerMWSA
-
-# disscussion
-# from model.dcformer_disscuss_mog_fusion_head import DCFormerMWSAMoGFusionHead
-# from model.dcformer_dissucss_multisource_proj import DCFormerMWSAMultiSourceProj
-
-# from model.LFormer import AttnFuseMain
-# from model.lformer_reduced_swin_attn import AttnFuseMain
-# from model.lformer_ablation.LFormer_ablation_skip_attn import AttnFuseMain
-
-# from model.reciprocal_transformer import DCT
-
-# from model.panMamba_old import ConditionalNAFNet
-# from model.panMamba import ConditionalNAFNet
-# from model.panMamba_ablation.panMamba_only_conv_NAF import ConditionalNAFNet
-
-# from model.panRWKV_v2 import ConditionalNAFNet
-# from model.panRWKV_v3 import ConditionalNAFNet
-
-# from model.panRWKV_v4 import RWKVFusion
-
-# from model.panRWKV_v5_cross import RWKVCrossFusion
-# from model.panrwkv_v7_k1 import RWKVFusion
-    
-# from model.panrwkv_v8_cond_norm import RWKVFusion
-
-# from model.MGDN import MGFF
-
-
-# from model.MIMO_SST import Net
-# from model.panmamba_zhouman import Net
-
-# from model.panRWKV import ConditionalNAFNet
-
-# others
-# from model.GPPNN import GPPNN
 
 # ============================================== model registry ==================================
 
@@ -220,5 +111,6 @@ def build_model_from_name(cfg: OmegaConf):
     return model
 
 # ==============================================
+
 
 
